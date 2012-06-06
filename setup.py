a55b5fdab7e4a714be6ca44db4b8da2320daae7f
Gonzalo Almeida <flecox@gmail.com>
2012-06-06 12:55:20 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
+
+setup(name='collective.rating',
+      version=version,
+      description="",
+      long_description=long_description,
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='rating ',
+      author='Gonzalo Almeida',
+      author_email='flecox@ravvit.net',
+      url='https://github.com/collective/collective.rating',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.app.dexterity>=1.2.1',
+        "five.grok",
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ src/collective/rating/interfaces.py
@@ -0,0 +1,20 @@
+from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """
+    Marker interface that defines a browser layer against which you can register views and viewlets.
+
+    When your theme is selected this layer becomes active and all overrides associated with
+    override default Plone render actions.
+    """
+
+
+class IAddonSpecific(Interface):
+    """
+    Marker interface that defines a browser layer against which you can register views and viewlets.
+
+    When your add-on is installed this layer becomes active and all overrides associated with
+    override default Plone render actions.
+    """
